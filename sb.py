8cfeaea46c6d99d1762c52c03fe1f87c04ab7942
fookwood <dangwenliang@gmail.com>
2012-08-10 20:04:28 +0800

    poj ok

--- sb.py
+++ sb.py
@@ -4,8 +4,8 @@ import urllib, httplib
 import os, sys, re, time
 
 
-username = "woodfook"
-password = "123"
+username = {"zoj":"woodfook","poj":"fkfkfk","hdu":"fkfkfk"}
+password = {"zoj":"123"     ,"poj":"fkfkfk","hdu":"fkfkfk"}
 oj = None
 problemid = None
 sourcefile = None
@@ -40,7 +40,7 @@ def prearg():
 def request(method,path,body="",headers={}):
 	global oj
 	conn = httplib.HTTPConnection(host[oj]);
-#conn.set_debuglevel(1)
+#	conn.set_debuglevel(1)
 	conn.request(method,path,body,headers)
 	rst = conn.getresponse()
 	strr = rst.read()
@@ -75,30 +75,52 @@ def getlanguage(code):
 	if l == "pl" :
 	 	return "perl"
 
-def readfile(code):
-	"""return source code in source file
-	"""
-	f = open(code,"r")
-	text = f.read()
-	f.close()
-	return text;
-
-def getcookiezoj():
+def getcookie():
 	"""simulate 'POST' to get cookies
 	"""
-	params = { "handle":username, "password":password }
+	params = None
+	path = None
+	if oj == "zoj":
+		params = { "handle":username[oj], "password":password[oj]}
+		path = "/onlinejudge/login.do"
+	if oj == "poj":
+		params = {"user_id1":username[oj],"password1":password[oj],"B1":"login","url":"/"}
+		path = "/login"
+	if oj == "hdu":
+		params = {"username":username[oj],"userpass":password[oj],"login":"Sign In"}
+		path = "/userloginex.php?action=login"
+	
 	params = urllib.urlencode( params )
 	header = {"Content-Type":"application/x-www-form-urlencoded",
 			   "Content-Length":str( len(params) ) } 
-	rst=request("POST","/onlinejudge/login.do",params,header)["response"]
+	rst=request("POST",path,params,header)["response"]
 	if rst.status == httplib.OK:
 		D("Is your username && password all right?\n")
 	strr = rst.getheader("Set-Cookie").split(" ");
-	return strr[2]
+	if oj == "zoj":
+		return strr[2]
+	return strr[0]
+
+def submitpoj( problemid, lang, code ):
+	path = "/submit"
+	params = urllib.urlencode( {"problem_id":problemid,
+								"language":1,
+								"source":code } )
+	header = {"Cookie":getcookie(),
+	          "Content-Type":"application/x-www-form-urlencoded",
+	          "Content-Length":str(len(params))}
+	request("POST","/submit",params,header)
+	status = ""
+	while status == "Waiting" or status == "Compiling" or status == "Running & Judging" or status == "":
+		strr = request("GET","/status?user_id="+username[oj])["string"].split("\n")[2]
+		temp = strr[strr.find("font"):].split("<")[0].split(">")[1]
+		if temp != status:
+			print temp
+		status = temp
 
 def submitzoj( problemid, lang, code ):
 	path = "/onlinejudge/showProblem.do?problemCode=";
-	header = {"Cookie":getcookiezoj()+" last_language=1" }
+	header = {"Cookie":getcookie() }
 	temp = request("GET",path+problemid,headers=header)
 	if temp["response"].status != httplib.OK:
 		D("Can't get zoj problemn\n")
@@ -138,4 +160,4 @@ def submitzoj( problemid, lang, code ):
 # connect to server, login, submit code
 if __name__ == "__main__":
 	prearg()
-	submitzoj(problemid,lang,sourcecode)	
+	submitpoj(problemid,lang,sourcecode)	
